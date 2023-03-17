385a16b34f4a1193bbe7fc6b8f9d63e2ec661aa2
Pradeep_Ranasinghe <pradeep@climatesi.com>
2023-03-17 08:57:30 +0530

    prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: true,
-  baseUrlAPI: 'http://localhost:7080',
-  esbaseUrlAPI: 'http://localhost:7090',
-  authBaseUrlAPI: 'http://localhost:7090',
-  baseUrlAPIDocUploadAPI: 'http://localhost:7081/document/upload2',
+  baseUrlAPI: 'http://65.0.7.85:7080',
+  esbaseUrlAPI: 'http://65.0.7.85:7090',
+  authBaseUrlAPI: 'http://65.0.7.85:7090',
+  baseUrlAPIDocUploadAPI: 'http://65.0.7.85:7081/document/upload2',
   baseUrlJsonFile: 'http://35.154.205.109:3600/filename',
-  baseUrlAPIDocUploadAnonymousAPI: 'http://localhost:7081/document/upload2'
+  baseUrlAPIDocUploadAnonymousAPI: 'http://65.0.7.85:7081/document/upload2'
 };
 // baseUrlAPI: 'http://3.110.188.89:7080',
 /*
