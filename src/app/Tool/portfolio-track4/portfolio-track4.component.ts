0a7d30b6f621f656d18e2a20cd1e713d388069ed
thilanmadhumadhawa3 <thilanmadhumadhawa3@gmail.com>
2023-06-01 12:49:59 +0530

    add indirect path

--- src/app/Tool/portfolio-track4/portfolio-track4.component.ts
+++ src/app/Tool/portfolio-track4/portfolio-track4.component.ts
@@ -336,41 +336,77 @@ export class PortfolioTrack4Component implements OnInit {
 
   onsubmit(form: NgForm) {
 
-    let finalArray = this.processData.concat(this.outcomeData)
-    finalArray.map(x => x.data.map(y => y.assessment = this.mainAssessment))
-    // finalArray.map(x=>x.data.map(y=>y.investorTool=this.mainAssessment))
-    console.log("finalArray", finalArray)
-    //@ts-ignore
-    this.investorToolControllerproxy.createFinalAssessment(finalArray)
-      .subscribe(_res => {
-        console.log("res final", _res)
+    console.log("assesssssssss", this.assessment)
+    if(this.assessment.assessment_approach === 'Direct'){
+      console.log("Directttt")
+      let finalArray = this.processData.concat(this.outcomeData)
+      finalArray.map(x => x.data.map(y => y.assessment = this.mainAssessment))
+      // finalArray.map(x=>x.data.map(y=>y.investorTool=this.mainAssessment))
+      console.log("finalArray", finalArray)
+      //@ts-ignore
+      this.investorToolControllerproxy.createFinalAssessment(finalArray)
+        .subscribe(_res => {
+          console.log("res final", _res)
 
-        console.log(_res)
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Success',
-          detail: 'Assessment created successfully',
-          closable: true,
+          console.log(_res)
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Assessment created successfully',
+            closable: true,
+          })
+          this.showResults();
+          // this.isSavedAssessment = true
+          // this.onCategoryTabChange('', this.tabView);
+
+
+          // form.reset();
+        }, error => {
+          console.log(error)
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Assessment detail saving failed',
+            closable: true,
+          })
         })
-        this.showResults();
-        // this.isSavedAssessment = true
-        // this.onCategoryTabChange('', this.tabView);
 
 
-        // form.reset();
-      }, error => {
-        console.log(error)
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'Assessment detail saving failed',
-          closable: true,
+     // console.log("+++++++++++", this.processData)
+     // console.log("-----------", this.outcomeData)
+    }
+    else{
+      console.log("Indirectttt")
+      let finalArray = this.processData.concat(this.outcomeData)
+      finalArray.map(x => x.data.map(y => y.assessment = this.mainAssessment))
+      // finalArray.map(x=>x.data.map(y=>y.investorTool=this.mainAssessment))
+      console.log("finalArray", finalArray)
+      //@ts-ignore
+      this.investorToolControllerproxy.createFinalAssessmentIndirect(finalArray)
+        .subscribe(_res => {
+          console.log("res final", _res)
+
+          console.log(_res)
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Assessment created successfully',
+            closable: true,
+          })
+          this.showResults();
+
+        }, error => {
+          console.log(error)
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Assessment detail saving failed',
+            closable: true,
+          })
         })
-      })
+    }
 
 
-    console.log("+++++++++++", this.processData)
-    console.log("-----------", this.outcomeData)
   }
 
 
