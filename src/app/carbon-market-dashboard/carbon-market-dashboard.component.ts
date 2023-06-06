2887aa1452dbf71634bd9d8926326a165eece152
Dilanka Ekanayake <dilanka@climatesi.com>
2023-06-06 16:43:11 +0530

    cm-dashboard

--- src/app/carbon-market-dashboard/carbon-market-dashboard.component.ts
+++ src/app/carbon-market-dashboard/carbon-market-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
-import { ClimateAction, ProjectControllerServiceProxy } from 'shared/service-proxies/service-proxies';
+import { AssessmentCMDetailControllerServiceProxy, ClimateAction, ProjectControllerServiceProxy } from 'shared/service-proxies/service-proxies';
 
 @Component({
   selector: 'app-carbon-market-dashboard',
@@ -11,6 +11,7 @@ export class CarbonMarketDashboardComponent implements OnInit {
 
   constructor(
     private projectProxy: ProjectControllerServiceProxy,
+    private assessmentCMProxy:AssessmentCMDetailControllerServiceProxy,
   ) { }
 
  // @ViewChild('canvas', { static: false }) canvas: ElementRef;
@@ -28,6 +29,21 @@ export class CarbonMarketDashboardComponent implements OnInit {
     y:number,
     data:string,
   }[]=[];
+
+  CMsectorCount: {
+    sectoral_boundary:number
+    average_tc_value:string,
+    
+    }[];
+
+CMPrerequiste: {
+  sector:string,
+  count:number
+  }[];
+
+
+  CMBarChart:any =[];
+  pieChartCM:any=[];
 
   ngOnInit(): void {
 
@@ -57,6 +73,19 @@ export class CarbonMarketDashboardComponent implements OnInit {
       console.log("aaa", this.tcData);
       console.log("bbb", this.tcTableData);
     });
+
+    this.assessmentCMProxy.getSectorCount().subscribe((res: any) => {
+      console.log("CMsectorCount",res)
+      this.CMsectorCount = res
+       this.viewCMBarChart();
+    })
+
+    this.assessmentCMProxy.getPrerequisite().subscribe((res:any)=>{
+      
+      this.CMPrerequiste=res
+      console.log("CMPrerequiste",res)
+      this.viewPieChartCM();
+    })
 
   }
 
@@ -150,6 +179,181 @@ export class CarbonMarketDashboardComponent implements OnInit {
       colors.push(randomColor);
     }
     return colors;
+  }
+
+  viewCMBarChart(){
+    let label =this.CMsectorCount.map((item) => item.sectoral_boundary);
+    let data =this.CMsectorCount.map((item) => item.average_tc_value);
+    console.log("label",label,"data",data)
+    this.CMBarChart =new Chart('CMBarCahart', {
+      type: 'bar',
+     
+      data: {
+        labels: label, 
+        datasets: [{
+          label: 'Bar Chart',
+          data: data, 
+          backgroundColor: [
+            'rgba(153, 102, 255, 1)',
+            'rgba(75, 192, 192,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(123, 122, 125, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 205, 86, 1)',
+            'rgba(255, 99, 132, 1)',
+            
+          ],
+          borderColor:[
+            'rgba(153, 102, 255, 1)',
+            'rgba(75, 192, 192,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(123, 122, 125, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 205, 86, 1)',
+            'rgba(255, 99, 132, 1)',],
+         
+          borderWidth: 1
+        }]
+      },
+      options:{
+        scales: {
+          x: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Sectors',
+              font: {
+                size: 16,
+                weight: 'bold',
+                
+              }
+            }
+          },
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Average Transformational Change (%)',
+              font: {
+                size: 14,
+                weight: 'bold',
+                
+              }
+            }
+          }
+        },
+        plugins:{
+          legend: {
+            display: false
+          },
+          tooltip:{
+            position:'average',
+            boxWidth:10,
+            callbacks:{
+              
+              label:(context)=>{ 
+               
+                return[
+                  
+                  `Average TC value: ${data[context.dataIndex]}`,
+                ];
+               }
+            },
+            backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
+              titleFont: {
+                size: 14,
+                weight: 'bold'
+              },
+              bodyFont: {
+                size: 14
+              },
+              displayColors: true, // Hide the color box in the tooltip
+              bodyAlign: 'left'
+          }
+        }
+        
+      }
+  });
+  
+  }
+  
+  
+  viewPieChartCM(){
+    const labels = this.CMPrerequiste.map((item) => item.sector);
+    let counts:number[] = this.CMPrerequiste.map((item) => item.count);
+    const total = counts.reduce((acc, val) => acc + val, 0);
+    const percentages = counts.map(count => ((count / total) * 100).toFixed(2));
+    this.pieChartCM =new Chart('pieChartCM', {
+      type: 'pie',
+     
+      data: {
+        labels: labels,
+        datasets: [{
+          data: counts,
+          backgroundColor: ['rgba(153, 102, 255, 1)','black'],
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins:{
+          legend:{
+            position: 'bottom',
+            labels: {
+              padding: 20
+            }
+          },
+          datalabels: {
+            color: '#fff',
+            font: {
+              size: 12
+            },
+            formatter: (value, ctx) => {
+              const label = ctx.chart.data.labels![ctx.dataIndex];
+              const percentage = percentages[ctx.dataIndex];
+              return `${label}: ${value} (${percentage}%)`;
+            },
+            
+          },
+          tooltip:{
+            position:'average',
+            boxWidth:10,
+            callbacks:{
+              
+              label:(ctx)=>{ 
+                let sum = 0;
+                let array =counts
+                array.forEach((number) => {
+                  sum += Number(number);
+                });
+                // console.log(sum, counts[ctx.dataIndex])
+                let percentage = (counts[ctx.dataIndex]*100 / sum).toFixed(2)+"%";
+               
+                return[
+                  ` ${labels[ctx.dataIndex]}`,
+                  `Count: ${counts[ctx.dataIndex]}`,
+                  `Percentage: ${percentage}`
+                ];
+               }
+            },
+            backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
+              titleFont: {
+                size: 14,
+                weight: 'bold'
+              },
+              bodyFont: {
+                size: 14
+              },
+              displayColors: true, // Hide the color box in the tooltip
+              bodyAlign: 'left'
+              
+          }
+       }
+         
+      },
+    
+  });
+  
   }
 
 
