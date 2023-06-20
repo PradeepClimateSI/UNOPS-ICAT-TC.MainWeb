f25c187d30bc67849cdb810cbdd5f4545f3d9ad3
Dilanka Ekanayake <dilanka@climatesi.com>
2023-06-20 14:54:03 +0530

    p-dashboard

--- src/app/portfolio-dashboard/portfolio-dashboard.component.ts
+++ src/app/portfolio-dashboard/portfolio-dashboard.component.ts
@@ -29,6 +29,11 @@ export class PortfolioDashboardComponent implements OnInit {
 
   averageTCValue:any;
 
+
+  /////
+  portfolioPieChart:any=[];
+  portfolioBarChart:any =[];
+
   ngOnInit(): void {
     this.averageTCValue =63.78
     this.tool = 'Portfolio Tool'
@@ -285,5 +290,191 @@ export class PortfolioDashboardComponent implements OnInit {
       },
     });
   }
+
+  // viewPortfolioPieChart(){
+  //   const labels = this.sectorCount.map((item) => item.sector);
+  //   let counts:number[] = this.sectorCount.map((item) => item.count);
+  //   const total = counts.reduce((acc, val) => acc + val, 0);
+  //   const percentages = counts.map(count => ((count / total) * 100).toFixed(2));
+  //   this.portfolioPieChart =new Chart('portfolioPieChart', {
+  //     type: 'pie',
+
+  //     data: {
+  //       labels: labels,
+  //       datasets: [{
+  //         data: counts,
+  //         backgroundColor: [
+  //           'rgba(153, 102, 255, 1)',
+  //           'rgba(75, 192, 192,1)',
+  //           'rgba(54, 162, 235, 1)',
+  //           'rgba(123, 122, 125, 1)',
+  //           'rgba(255, 99, 132, 1)',
+  //           'rgba(255, 205, 86, 1)',
+  //           'rgba(255, 99, 132, 1)',
+
+  //         ],
+         
+  //       }]
+  //     },
+  //     options: {
+  //       responsive: true,
+  //       maintainAspectRatio: false,
+  //       plugins:{
+  //         legend:{
+  //           position: 'bottom',
+  //           labels: {
+  //             padding: 20
+  //           }
+  //         },
+  //         datalabels: {
+  //           color: '#fff',
+  //           font: {
+  //             size: 12
+  //           },
+  //           formatter: (value, ctx) => {
+  //             const label = ctx.chart.data.labels![ctx.dataIndex];
+  //             const percentage = percentages[ctx.dataIndex];
+  //             return `${label}: ${value} (${percentage}%)`;
+  //           },
+
+  //         },
+  //         tooltip:{
+  //           position:'average',
+  //           boxWidth:10,
+  //           callbacks:{
+              
+  //             label:(ctx)=>{ 
+  //               // console.log(ctx)
+  //               // let sum = ctx.dataset._meta[0].total;
+  //               // let percentage = (value * 100 / sum).toFixed(2) + "%";
+  //               // return percentage;
+  //               let sum = 0;
+  //               let array =counts
+  //               array.forEach((number) => {
+  //                 sum += Number(number);
+  //               });
+  //               // console.log(sum, counts[ctx.dataIndex])
+  //               let percentage = (counts[ctx.dataIndex]*100 / sum).toFixed(2)+"%";
+
+  //               return[
+  //                 `Sector: ${labels[ctx.dataIndex]}`,
+  //                 `Count: ${counts[ctx.dataIndex]}`,
+  //                 `Percentage: ${percentage}`
+  //               ];
+  //              }
+  //           },
+  //           backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
+  //             titleFont: {
+  //               size: 14,
+  //               weight: 'bold'
+  //             },
+  //             bodyFont: {
+  //               size: 14
+  //             },
+  //             displayColors: true, // Hide the color box in the tooltip
+  //             bodyAlign: 'left'
+  //         }
+  //      }
+
+  //     },
+
+  // });
+
+  // }
+  // viewPortfolioBarChart(){
+  //   let label =this.CMsectorCount.map((item) => item.sectoral_boundary);
+  //   let data =this.CMsectorCount.map((item) => item.average_tc_value);
+  //   console.log("label",label,"data",data)
+  //   this.portfolioBarChart =new Chart('portfolioBarChart', {
+  //     type: 'bar',
+
+  //     data: {
+  //       labels: label,
+  //       datasets: [{
+  //         label: 'Bar Chart',
+  //         data: data,
+  //         backgroundColor: [
+  //           'rgba(153, 102, 255, 1)',
+  //           'rgba(75, 192, 192,1)',
+  //           'rgba(54, 162, 235, 1)',
+  //           'rgba(123, 122, 125, 1)',
+  //           'rgba(255, 99, 132, 1)',
+  //           'rgba(255, 205, 86, 1)',
+  //           'rgba(255, 99, 132, 1)',
+
+  //         ],
+  //         borderColor:[
+  //           'rgba(153, 102, 255, 1)',
+  //           'rgba(75, 192, 192,1)',
+  //           'rgba(54, 162, 235, 1)',
+  //           'rgba(123, 122, 125, 1)',
+  //           'rgba(255, 99, 132, 1)',
+  //           'rgba(255, 205, 86, 1)',
+  //           'rgba(255, 99, 132, 1)',],
+
+  //         borderWidth: 1
+  //       }]
+  //     },
+  //     options:{
+  //       scales: {
+  //         x: {
+  //           beginAtZero: true,
+  //           title: {
+  //             display: true,
+  //             text: 'Sectors',
+  //             font: {
+  //               size: 16,
+  //               weight: 'bold',
+
+  //             }
+  //           }
+  //         },
+  //         y: {
+  //           beginAtZero: true,
+  //           title: {
+  //             display: true,
+  //             text: 'Average Transformational Change (%)',
+  //             font: {
+  //               size: 12,
+  //               weight: 'bold',
+
+  //             }
+  //           }
+  //         }
+  //       },
+  //       plugins:{
+  //         legend: {
+  //           display: false
+  //         },
+  //         tooltip:{
+  //           position:'average',
+  //           boxWidth:10,
+  //           callbacks:{
+
+  //             label:(context)=>{
+
+  //               return[
+
+  //                 `Average TC value: ${data[context.dataIndex]}`,
+  //               ];
+  //              }
+  //           },
+  //           backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
+  //             titleFont: {
+  //               size: 14,
+  //               weight: 'bold'
+  //             },
+  //             bodyFont: {
+  //               size: 14
+  //             },
+  //             displayColors: true, // Hide the color box in the tooltip
+  //             bodyAlign: 'left'
+  //         }
+  //       }
+
+  //     }
+  // });
+
+  // }
 
 }
