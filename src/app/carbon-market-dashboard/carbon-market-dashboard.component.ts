842a5261d7ad401e4e09909c65e0d8dea10b3b4b
Dilanka Ekanayake <dilanka@climatesi.com>
2023-09-19 12:38:43 +0530

    comment cm-dashboard

--- src/app/carbon-market-dashboard/carbon-market-dashboard.component.ts
+++ src/app/carbon-market-dashboard/carbon-market-dashboard.component.ts
@@ -68,40 +68,40 @@ CMPrerequiste: {
 
     this.tool = 'Carbon Market Tool';
 
-    this.methassess.getTCForTool(this.tool).subscribe((res: any) => {
-      console.log("kkkkk : ", res)
+    // this.methassess.getTCForTool(this.tool).subscribe((res: any) => {
+    //   console.log("kkkkk : ", res)
 
-      this.interventions = res;
-    //  console.log("policyList:", this.interventions);
-      this.averageTCValue= (res.reduce((total:number, next:any) => total + Number(next.y), 0) / res.length).toFixed(2);
-      console.log( "averageTCValue :",this.averageTCValue)
+    //   this.interventions = res;
+    // //  console.log("policyList:", this.interventions);
+    //   this.averageTCValue= (res.reduce((total:number, next:any) => total + Number(next.y), 0) / res.length).toFixed(2);
+    //   console.log( "averageTCValue :",this.averageTCValue)
 
-      // Sort interventions based on id in descending order
-      this.interventions.sort((a: { id: number; }, b: { id: number; }) => b.id - a.id);
+    //   // Sort interventions based on id in descending order
+    //   this.interventions.sort((a: { id: number; }, b: { id: number; }) => b.id - a.id);
 
-      // Get the last 5 interventions
-      const lastFiveInterventions = this.interventions.slice(0,5);
+    //   // Get the last 5 interventions
+    //   const lastFiveInterventions = this.interventions.slice(0,5);
 
-      this.tcData = lastFiveInterventions
-     /*  this.tcData = lastFiveInterventions.map(intervention => ({
-        y: intervention?.tc_value,
-        x: intervention?.intervention_id,
-        data: intervention?.data
-      }));
+    //   this.tcData = lastFiveInterventions
+    //  /*  this.tcData = lastFiveInterventions.map(intervention => ({
+    //     y: intervention?.tc_value,
+    //     x: intervention?.intervention_id,
+    //     data: intervention?.data
+    //   }));
 
-      this.tcTableData = this.interventions.map(intervention => ({
-        y: intervention?.tc_value,
-        x: intervention?.intervention_id,
-        data: intervention?.policyName
-      })); */
+    //   this.tcTableData = this.interventions.map(intervention => ({
+    //     y: intervention?.tc_value,
+    //     x: intervention?.intervention_id,
+    //     data: intervention?.policyName
+    //   })); */
 
-      this.tcTableData = this.interventions
+    //   this.tcTableData = this.interventions
 
-      this.viewMainChart();
-      console.log("aaa", this.tcData);
-      console.log("bbb", this.tcTableData);
+    //   // this.viewMainChart();
+    //   console.log("aaa", this.tcData);
+    //   console.log("bbb", this.tcTableData);
 
-    });
+    // });
 
 
 /*     this.projectProxy.findAllPolicies().subscribe((res: any) => {
@@ -131,14 +131,14 @@ CMPrerequiste: {
       console.log("bbb", this.tcTableData);
     }); */
 
-    this.assessmentCMProxy.getSectorCount().subscribe((res: any) => {
-      console.log("CMsectorCount",res)
-      this.CMsectorCount = res
-      setTimeout(() => {
-        this.viewCMBarChart();
-      }, 500);
+    // this.assessmentCMProxy.getSectorCount().subscribe((res: any) => {
+    //   console.log("CMsectorCount",res)
+    //   this.CMsectorCount = res
+    //   setTimeout(() => {
+    //     this.viewCMBarChart();
+    //   }, 500);
        
-    })
+    // })
 
     this.assessmentCMProxy.getPrerequisite().subscribe((res:any)=>{
 
@@ -150,187 +150,186 @@ CMPrerequiste: {
       
     })
 
-    this.investorProxy.getSectorCountByTool(this.tool).subscribe((res: any) => {
-      this.sectorCount = res
-      console.log("sectorcount",this.sectorCount)
-      setTimeout(() => {
-        this.viewPieChart()
-      }, 100);
+    // this.investorProxy.getSectorCountByTool(this.tool).subscribe((res: any) => {
+    //   this.sectorCount = res
+    //   console.log("sectorcount",this.sectorCount)
+    //   setTimeout(() => {
+    //   }, 100);
       
-    });
+    // });
 
   }
 
-  viewPieChart(){
-    const labels = this.sectorCount.map((item) => item.sector);
-    let counts:number[] = this.sectorCount.map((item) => item.count);
-    const total = counts.reduce((acc, val) => acc + val, 0);
-    const percentages = counts.map(count => ((count / total) * 100).toFixed(2));
-    this.pieChart2 =new Chart('pieChart2', {
-      type: 'pie',
+  // viewPieChart(){
+  //   const labels = this.sectorCount.map((item) => item.sector);
+  //   let counts:number[] = this.sectorCount.map((item) => item.count);
+  //   const total = counts.reduce((acc, val) => acc + val, 0);
+  //   const percentages = counts.map(count => ((count / total) * 100).toFixed(2));
+  //   this.pieChart2 =new Chart('pieChart2', {
+  //     type: 'pie',
 
-      data: {
-        labels: labels,
-        datasets: [{
-          data: counts,
-          backgroundColor: [
-            'rgba(153, 102, 255, 1)',
-            'rgba(75, 192, 192,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(123, 122, 125, 1)',
-            'rgba(255, 99, 132, 1)',
-            'rgba(255, 205, 86, 1)',
-            'rgba(255, 99, 132, 1)',
+  //     data: {
+  //       labels: labels,
+  //       datasets: [{
+  //         data: counts,
+  //         backgroundColor: [
+  //           'rgba(153, 102, 255, 1)',
+  //           'rgba(75, 192, 192,1)',
+  //           'rgba(54, 162, 235, 1)',
+  //           'rgba(123, 122, 125, 1)',
+  //           'rgba(255, 99, 132, 1)',
+  //           'rgba(255, 205, 86, 1)',
+  //           'rgba(255, 99, 132, 1)',
 
-          ],
+  //         ],
 
-        }]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins:{
-          legend:{
-            position: 'bottom',
-            labels: {
-              padding: 20
-            }
-          },
-          datalabels: {
-            color: '#fff',
-            font: {
-              size: 12
-            },
-            formatter: (value, ctx) => {
-              const label = ctx.chart.data.labels![ctx.dataIndex];
-              const percentage = percentages[ctx.dataIndex];
-              return `${label}: ${value} (${percentage}%)`;
-            },
+  //       }]
+  //     },
+  //     options: {
+  //       responsive: true,
+  //       maintainAspectRatio: false,
+  //       plugins:{
+  //         legend:{
+  //           position: 'bottom',
+  //           labels: {
+  //             padding: 20
+  //           }
+  //         },
+  //         datalabels: {
+  //           color: '#fff',
+  //           font: {
+  //             size: 12
+  //           },
+  //           formatter: (value, ctx) => {
+  //             const label = ctx.chart.data.labels![ctx.dataIndex];
+  //             const percentage = percentages[ctx.dataIndex];
+  //             return `${label}: ${value} (${percentage}%)`;
+  //           },
 
-          },
-          tooltip:{
-            position:'average',
-            boxWidth:10,
-            callbacks:{
+  //         },
+  //         tooltip:{
+  //           position:'average',
+  //           boxWidth:10,
+  //           callbacks:{
 
-              label:(ctx)=>{
-                // console.log(ctx)
-                // let sum = ctx.dataset._meta[0].total;
-                // let percentage = (value * 100 / sum).toFixed(2) + "%";
-                // return percentage;
-                let sum = 0;
-                let array =counts
-                array.forEach((number) => {
-                  sum += Number(number);
-                });
-                // console.log(sum, counts[ctx.dataIndex])
-                let percentage = (counts[ctx.dataIndex]*100 / sum).toFixed(2)+"%";
+  //             label:(ctx)=>{
+  //               // console.log(ctx)
+  //               // let sum = ctx.dataset._meta[0].total;
+  //               // let percentage = (value * 100 / sum).toFixed(2) + "%";
+  //               // return percentage;
+  //               let sum = 0;
+  //               let array =counts
+  //               array.forEach((number) => {
+  //                 sum += Number(number);
+  //               });
+  //               // console.log(sum, counts[ctx.dataIndex])
+  //               let percentage = (counts[ctx.dataIndex]*100 / sum).toFixed(2)+"%";
 
-                return[
-                  `Sector: ${labels[ctx.dataIndex]}`,
-                  `Count: ${counts[ctx.dataIndex]}`,
-                  `Percentage: ${percentage}`
-                ];
-               }
-            },
-            backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
-              titleFont: {
-                size: 14,
-                weight: 'bold'
-              },
-              bodyFont: {
-                size: 14
-              },
-              displayColors: true, // Hide the color box in the tooltip
-              bodyAlign: 'left'
-          }
-       }
+  //               return[
+  //                 `Sector: ${labels[ctx.dataIndex]}`,
+  //                 `Count: ${counts[ctx.dataIndex]}`,
+  //                 `Percentage: ${percentage}`
+  //               ];
+  //              }
+  //           },
+  //           backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
+  //             titleFont: {
+  //               size: 14,
+  //               weight: 'bold'
+  //             },
+  //             bodyFont: {
+  //               size: 14
+  //             },
+  //             displayColors: true, // Hide the color box in the tooltip
+  //             bodyAlign: 'left'
+  //         }
+  //      }
 
-      },
+  //     },
 
-  });
+  // });
 
-  }
+  // }
 
-  viewMainChart(){
-   // const canvas = this.canvas.nativeElement.getContext('2d');
-    this.chart =new Chart('canvas', {
-      type: 'line',
+  // viewMainChart(){
+  //  // const canvas = this.canvas.nativeElement.getContext('2d');
+  //   this.chart =new Chart('canvas', {
+  //     type: 'line',
 
-      data: {
+  //     data: {
 
-          datasets: [
-            {
-              label:'TC change with investments',
-              data: this.tcData,
-              backgroundColor: 'blue',
-              borderColor: "#black",
-              pointRadius:10,
-              borderWidth: 0,
-          }],
-      },
-      options:{
-        scales: {
-          x: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Intervention ID',
-              font: {
-                size: 16,
-                weight: 'bold',
+  //         datasets: [
+  //           {
+  //             label:'TC change with investments',
+  //             data: this.tcData,
+  //             backgroundColor: 'blue',
+  //             borderColor: "#black",
+  //             pointRadius:10,
+  //             borderWidth: 0,
+  //         }],
+  //     },
+  //     options:{
+  //       scales: {
+  //         x: {
+  //           beginAtZero: true,
+  //           title: {
+  //             display: true,
+  //             text: 'Intervention ID',
+  //             font: {
+  //               size: 16,
+  //               weight: 'bold',
 
-              }
-            }
-          },
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Transformational Change (%)',
-              font: {
-                size: 16,
-                weight: 'bold',
+  //             }
+  //           }
+  //         },
+  //         y: {
+  //           beginAtZero: true,
+  //           title: {
+  //             display: true,
+  //             text: 'Transformational Change (%)',
+  //             font: {
+  //               size: 16,
+  //               weight: 'bold',
 
-              }
-            }
-          }
-        },
-        plugins:{
-          legend: {
-            display: false
-          },
-          tooltip:{
-            position:'average',
-            boxWidth:10,
-            callbacks:{
+  //             }
+  //           }
+  //         }
+  //       },
+  //       plugins:{
+  //         legend: {
+  //           display: false
+  //         },
+  //         tooltip:{
+  //           position:'average',
+  //           boxWidth:10,
+  //           callbacks:{
 
-              label:(context)=>{
+  //             label:(context)=>{
 
-                return[
-                  `Intervention: ${this.tcData[context.dataIndex].data}`,
-                  `Intervention Id: ${this.tcData[context.dataIndex].x}`,
-                  `TC value: ${this.tcData[context.dataIndex].y}%`
-                ];
-               }
-            },
-            backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
-              titleFont: {
-                size: 14,
-                weight: 'bold'
-              },
-              bodyFont: {
-                size: 14
-              },
-              displayColors: true, // Hide the color box in the tooltip
-              bodyAlign: 'left'
-          }
-        }
+  //               return[
+  //                 `Intervention: ${this.tcData[context.dataIndex].data}`,
+  //                 `Intervention Id: ${this.tcData[context.dataIndex].x}`,
+  //                 `TC value: ${this.tcData[context.dataIndex].y}%`
+  //               ];
+  //              }
+  //           },
+  //           backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
+  //             titleFont: {
+  //               size: 14,
+  //               weight: 'bold'
+  //             },
+  //             bodyFont: {
+  //               size: 14
+  //             },
+  //             displayColors: true, // Hide the color box in the tooltip
+  //             bodyAlign: 'left'
+  //         }
+  //       }
 
-      }
-  });
+  //     }
+  // });
 
-  }
+  // }
 
   getRandomColors(count: number): string[] {
     const colors: string[] = [];
@@ -341,101 +340,101 @@ CMPrerequiste: {
     return colors;
   }
 
-  viewCMBarChart(){
-    let label =this.CMsectorCount.map((item) => item.sectoral_boundary);
-    let data =this.CMsectorCount.map((item) => item.average_tc_value);
-    console.log("label",label,"data",data)
-    this.CMBarChart =new Chart('CMBarCahart', {
-      type: 'bar',
+  // viewCMBarChart(){
+  //   let label =this.CMsectorCount.map((item) => item.sectoral_boundary);
+  //   let data =this.CMsectorCount.map((item) => item.average_tc_value);
+  //   console.log("label",label,"data",data)
+  //   this.CMBarChart =new Chart('CMBarCahart', {
+  //     type: 'bar',
 
-      data: {
-        labels: label,
-        datasets: [{
-          label: 'Bar Chart',
-          data: data,
-          backgroundColor: [
-            'rgba(153, 102, 255, 1)',
-            'rgba(75, 192, 192,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(123, 122, 125, 1)',
-            'rgba(255, 99, 132, 1)',
-            'rgba(255, 205, 86, 1)',
-            'rgba(255, 99, 132, 1)',
+  //     data: {
+  //       labels: label,
+  //       datasets: [{
+  //         label: 'Bar Chart',
+  //         data: data,
+  //         backgroundColor: [
+  //           'rgba(153, 102, 255, 1)',
+  //           'rgba(75, 192, 192,1)',
+  //           'rgba(54, 162, 235, 1)',
+  //           'rgba(123, 122, 125, 1)',
+  //           'rgba(255, 99, 132, 1)',
+  //           'rgba(255, 205, 86, 1)',
+  //           'rgba(255, 99, 132, 1)',
 
-          ],
-          borderColor:[
-            'rgba(153, 102, 255, 1)',
-            'rgba(75, 192, 192,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(123, 122, 125, 1)',
-            'rgba(255, 99, 132, 1)',
-            'rgba(255, 205, 86, 1)',
-            'rgba(255, 99, 132, 1)',],
+  //         ],
+  //         borderColor:[
+  //           'rgba(153, 102, 255, 1)',
+  //           'rgba(75, 192, 192,1)',
+  //           'rgba(54, 162, 235, 1)',
+  //           'rgba(123, 122, 125, 1)',
+  //           'rgba(255, 99, 132, 1)',
+  //           'rgba(255, 205, 86, 1)',
+  //           'rgba(255, 99, 132, 1)',],
 
-          borderWidth: 1
-        }]
-      },
-      options:{
-        scales: {
-          x: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Sectors',
-              font: {
-                size: 16,
-                weight: 'bold',
+  //         borderWidth: 1
+  //       }]
+  //     },
+  //     options:{
+  //       scales: {
+  //         x: {
+  //           beginAtZero: true,
+  //           title: {
+  //             display: true,
+  //             text: 'Sectors',
+  //             font: {
+  //               size: 16,
+  //               weight: 'bold',
 
-              }
-            }
-          },
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Average Transformational Change (%)',
-              font: {
-                size: 12,
-                weight: 'bold',
+  //             }
+  //           }
+  //         },
+  //         y: {
+  //           beginAtZero: true,
+  //           title: {
+  //             display: true,
+  //             text: 'Average Transformational Change (%)',
+  //             font: {
+  //               size: 12,
+  //               weight: 'bold',
 
-              }
-            }
-          }
-        },
-        plugins:{
-          legend: {
-            display: false
-          },
-          tooltip:{
-            position:'average',
-            boxWidth:10,
-            callbacks:{
+  //             }
+  //           }
+  //         }
+  //       },
+  //       plugins:{
+  //         legend: {
+  //           display: false
+  //         },
+  //         tooltip:{
+  //           position:'average',
+  //           boxWidth:10,
+  //           callbacks:{
 
-              label:(context)=>{
+  //             label:(context)=>{
 
-                return[
+  //               return[
 
-                  `Average TC value: ${data[context.dataIndex]}`,
-                ];
-               }
-            },
-            backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
-              titleFont: {
-                size: 14,
-                weight: 'bold'
-              },
-              bodyFont: {
-                size: 14
-              },
-              displayColors: true, // Hide the color box in the tooltip
-              bodyAlign: 'left'
-          }
-        }
+  //                 `Average TC value: ${data[context.dataIndex]}`,
+  //               ];
+  //              }
+  //           },
+  //           backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set the background color of the tooltip box
+  //             titleFont: {
+  //               size: 14,
+  //               weight: 'bold'
+  //             },
+  //             bodyFont: {
+  //               size: 14
+  //             },
+  //             displayColors: true, // Hide the color box in the tooltip
+  //             bodyAlign: 'left'
+  //         }
+  //       }
 
-      }
-  });
+  //     }
+  // });
 
-  }
+  // }
 
 
   viewPieChartCM(){
