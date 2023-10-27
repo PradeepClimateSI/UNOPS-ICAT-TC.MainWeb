a6752236a8e575dc5db49528b4c71e89eb1451e0
Thilan Madhumadhawa <thilan@climatesi.com>
2023-10-27 18:46:52 +0530

    save func

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -212,13 +212,13 @@ const routes: Routes = [
     canActivate: [],
     data: {}
   },
-  {
+ /*  {
     path: 'portfolio-tool',
     component: PortfolioComponent,
     loadChildren: () => import('../dashboard/dashboard.module').then((m) => m.DashboardModule),
     canActivate: [],
     data: {}
-  },
+  }, */
   {
     path: 'investor-tool',
     component: InvestorComponent,
