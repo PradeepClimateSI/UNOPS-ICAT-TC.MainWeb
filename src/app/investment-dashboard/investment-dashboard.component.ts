3ab79c09fdf1623beff87f365d6f521c84c7fc84
Dilanka Ekanayake <dilanka@climatesi.com>
2023-11-03 19:59:00 +0530

    all toll dashboard

--- src/app/investment-dashboard/investment-dashboard.component.ts
+++ src/app/investment-dashboard/investment-dashboard.component.ts
@@ -236,7 +236,18 @@ export class InvestmentDashboardComponent implements OnInit,AfterViewInit {
             'rgba(123, 122, 125, 1)',
             'rgba(255, 99, 132, 1)',
             'rgba(255, 205, 86, 1)',
-            'rgba(255, 99, 132, 1)',
+            'rgba(70, 51, 102, 1)',
+            'rgba(40, 102, 102, 1)',
+            'rgba(27, 74, 107, 1)',
+            'rgba(75, 74, 77, 1)',
+            'rgba(121, 27, 53, 1)',
+            'rgba(121, 98, 20, 1)',
+            'rgba(51, 0, 51, 1)',
+            'rgba(25, 25, 112, 1)',
+            'rgba(139, 0, 0, 1)',
+            'rgba(0, 0, 139, 1)',
+            'rgba(47, 79, 79, 1)',
+            'rgba(139, 69, 19, 1)'
 
           ],
          
