bc8d7d457a58898f0d4637ad435e217bbf0d5a4a
Pradeep_Ranasinghe <pradeep@climatesi.com>
2024-01-04 12:40:23 +0530

    envirment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   baseUrlPMU: 'http://localhost:7080/pmu/login',
   baseUrlAPI: 'http://localhost:7080',
   esbaseUrlAPI: 'http://localhost:7070',
-  authBaseUrlAPI: 'https://icat-tc-tool.climatesi.com/auth-api',
+  authBaseUrlAPI: 'http://localhost:7090',
   baseUrlAPIDocUploadAPI: 'http://localhost:7080/document/upload2',
   baseUrlAPIDocUploadAnonymousAPI: 'http://localhost:7080/document/upload2',
   baseUrlAuditlog: 'http://localhost:7000',
