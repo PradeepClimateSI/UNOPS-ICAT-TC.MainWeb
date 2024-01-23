5a9879a99009633792d02323e41dd2ccfa17f10c
Dilanka Ekanayake <dilanka@climatesi.com>
2024-01-23 21:22:59 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
-  baseUrlAPI: 'https://52.66.203.70/web-api',
+  baseUrlAPI: 'https://tc.climatesi.com/web-api',
   esbaseUrlAPI: 'https://15.206.202.183:7090',
-  authBaseUrlAPI: 'https://52.66.203.70/auth-api',
-  baseUrlAPIDocUploadAPI: 'https://52.66.203.70/web-api/document/upload2',
+  authBaseUrlAPI: 'https://tc.climatesi.com/auth-api',
+  baseUrlAPIDocUploadAPI: 'https://tc.climatesi.com/web-api/document/upload2',
   
-  baseUrlAuditlog :  'https://52.66.203.70/audit-api',
+  baseUrlAuditlog :  'https://tc.climatesi.com/audit-api',
   baseUrlJsonFile: 'https://15.206.202.183:3600/filename',
-  baseUrlAPIDocUploadAnonymousAPI: 'https://52.66.203.70/web-api/document/upload2',
-  baseUrlExcelUpload:  'https://52.66.203.70/web-api/parameter/upload',
-  baseUrlPMU: 'https://52.66.203.70/pmu/login',
-  videoURL: 'https://52.66.203.70/web-api/video/'
+  baseUrlAPIDocUploadAnonymousAPI: 'https://tc.climatesi.com/web-api/document/upload2',
+  baseUrlExcelUpload:  'https://tc.climatesi.com/web-api/parameter/upload',
+  baseUrlPMU: 'https://tc.climatesi.com/pmu/login',
+  videoURL: 'https://tc.climatesi.com/web-api/video/'
 
 };
