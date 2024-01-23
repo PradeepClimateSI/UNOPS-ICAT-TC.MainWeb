3fef3d8ab1120abdac9d2479cc71efc5d3571772
Dilanka Ekanayake <dilanka@climatesi.com>
2024-01-23 17:22:41 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
-  baseUrlAPI: 'https://icat-tc-tool.climatesi.com/web-api',
-  esbaseUrlAPI: 'http://15.206.202.183:7090',
-  authBaseUrlAPI: 'https://icat-tc-tool.climatesi.com/auth-api',
-  baseUrlAPIDocUploadAPI: 'https://icat-tc-tool.climatesi.com/web-api/document/upload2',
+  baseUrlAPI: 'https://52.66.203.70/web-api',
+  esbaseUrlAPI: 'https://15.206.202.183:7090',
+  authBaseUrlAPI: 'https://52.66.203.70/auth-api',
+  baseUrlAPIDocUploadAPI: 'https://52.66.203.70/web-api/document/upload2',
   
-  baseUrlAuditlog :  'https://icat-tc-tool.climatesi.com/audit-api',
-  baseUrlJsonFile: 'http://15.206.202.183:3600/filename',
-  baseUrlAPIDocUploadAnonymousAPI: 'https://icat-tc-tool.climatesi.com/web-api/document/upload2',
-  baseUrlExcelUpload:  'https://icat-tc-tool.climatesi.com/web-api/parameter/upload',
-  baseUrlPMU: 'https://icat-tc-tool.climatesi.com/pmu/login',
-  videoURL: 'https://icat-tc-tool.climatesi.com/web-api/video/'
+  baseUrlAuditlog :  'https://52.66.203.70/audit-api',
+  baseUrlJsonFile: 'https://15.206.202.183:3600/filename',
+  baseUrlAPIDocUploadAnonymousAPI: 'https://52.66.203.70/web-api/document/upload2',
+  baseUrlExcelUpload:  'https://52.66.203.70/web-api/parameter/upload',
+  baseUrlPMU: 'https://52.66.203.70/pmu/login',
+  videoURL: 'https://52.66.203.70/web-api/video/'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,16 @@
 
 export const environment = {
   production: false,
-  baseUrlPMU: 'http://localhost:7080/pmu/login',
-  baseUrlAPI: '',
-  authBaseUrlAPI: '',
-  baseUrlAPIDocUploadAPI: 'http://localhost:7080/document/upload2',
-  baseUrlAuditlog: 'http://localhost:7000',
-  videoURL: '',
-  baseUrlExcelUpload :''
-
+  baseUrlAPI: 'https://52.66.203.70/web-api',
+  esbaseUrlAPI: 'https://15.206.202.183:7090',
+  authBaseUrlAPI: 'https://52.66.203.70/auth-api',
+  baseUrlAPIDocUploadAPI: 'https://52.66.203.70/web-api/document/upload2',
+  
+  baseUrlAuditlog :  'https://52.66.203.70/audit-api',
+  baseUrlJsonFile: 'https://15.206.202.183:3600/filename',
+  baseUrlAPIDocUploadAnonymousAPI: 'https://52.66.203.70/web-api/document/upload2',
+  baseUrlExcelUpload:  'https://52.66.203.70/web-api/parameter/upload',
+  baseUrlPMU: 'https://52.66.203.70/pmu/login',
+  videoURL: 'https://52.66.203.70/web-api/video/'
 
 };
