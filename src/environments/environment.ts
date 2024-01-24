faed48c7ba2cbea4d86a0cd122f8a7262dbf33b4
Pradeep_Ranasinghe <pradeep@climatesi.com>
2024-01-24 11:23:29 +0530

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
 
 export const environment = {
   production: false,
-  baseUrlAPI: 'https://52.66.203.70/web-api',
-  esbaseUrlAPI: 'https://15.206.202.183:7090',
-  authBaseUrlAPI: 'https://52.66.203.70/auth-api',
-  baseUrlAPIDocUploadAPI: 'https://52.66.203.70/web-api/document/upload2',
+  baseUrlAPI: '',
+  authBaseUrlAPI: '',
+  baseUrlAPIDocUploadAPI: '',
   
-  baseUrlAuditlog :  'https://52.66.203.70/audit-api',
-  baseUrlJsonFile: 'https://15.206.202.183:3600/filename',
-  baseUrlAPIDocUploadAnonymousAPI: 'https://52.66.203.70/web-api/document/upload2',
-  baseUrlExcelUpload:  'https://52.66.203.70/web-api/parameter/upload',
-  baseUrlPMU: 'https://52.66.203.70/pmu/login',
-  videoURL: 'https://52.66.203.70/web-api/video/'
+  baseUrlAuditlog :  '',
+  baseUrlPMU: '',
+  videoURL: ''
 
 };
