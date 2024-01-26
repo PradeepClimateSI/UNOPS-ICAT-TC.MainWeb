5351b364d7ff502d3fad0f125453666bb9af6fbe
madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2024-01-26 16:32:05 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseUrlAPI: 'htts://tc-main.climatesi.com',
-  authBaseUrlAPI: 'htts://tc-auth.climatesi.com',
+  authBaseUrlAPI: 'https://tc-auth.climatesi.com',
   
   baseUrlAuditlog :  'https://tc-audit.climatesi.com',
   baseUrlPMU: 'https://tc-pmu-be.climatesi.com',
