fd1ad8f25300c0c0316d69528ddd2bda15034d56
Pradeep_Ranasinghe <pradeep@climatesi.com>
2024-01-29 15:56:23 +0530

    merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,7 @@ export const environment = {
   baseUrlAPI: 'https://tc-main.climatesi.com',
   authBaseUrlAPI: 'https://tc-auth.climatesi.com',
   baseUrlAPIDocUploadAPI: 'https://localhost/web/api/document/upload2',
-<<<<<<< HEAD
   baseUrlAuditlog :  'https://tc-audit.climatesi.com',
   baseUrlPMU: 'https://tc-pmu-be.climatesi.com',
   videoURL: 'https://sccv2.s3.ap-south-1.amazonaws.com/videos',
-=======
-  baseUrlAuditlog: 'https://tc.climatesi.com/audit-api',
-  videoURL: 'https://sccv2.s3.ap-south-1.amazonaws.com/videos'
->>>>>>> e1c8d347fb62bbb3da37408697dd750d6201ee93
 };
