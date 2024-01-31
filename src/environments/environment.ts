6256dbe72e232fde24ddefd412c28f1665cf9a3e
Pradeep_Ranasinghe <pradeep@climatesi.com>
2024-01-31 18:30:25 +0530

    clean code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,10 @@ export const environment = {
   production: false,
   baseUrlPMU: 'http://localhost:7080/pmu/login',
   baseUrlAPI: 'http://localhost:7080',
-  authBaseUrlAPI: 'https://tc.climatesi.com/auth-api',
+  authBaseUrlAPI: '',
   baseUrlAPIDocUploadAPI: 'http://localhost:7080/document/upload2',
-  baseUrlAuditlog: 'https://tc.climatesi.com/audit-api',
-  videoURL: 'https://sccv2.s3.ap-south-1.amazonaws.com/videos'
+  baseUrlAuditlog: '',
+  videoURL: ''
 
 
 };
