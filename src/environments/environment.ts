32188488e639e733f31298c23292e225b9add3ff
Kashul <kashul@climatesi.com>
2024-02-13 17:34:13 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   baseUrlPMU: 'http://localhost:7080/pmu/login',
   baseUrlAPI: 'http://localhost:7080',
-  authBaseUrlAPI: 'https://tc.climatesi.com/auth-api',
+  authBaseUrlAPI: 'http://localhost:7090',
   baseUrlAPIDocUploadAPI: 'http://localhost:7080/document/upload2',
   baseUrlAuditlog: 'https://tc.climatesi.com/audit-api',
   videoURL: 'https://sccv2.s3.ap-south-1.amazonaws.com/videos'
