43c18ade0a95cac151799ca3155d77e55425eb71
Pradeep_Ranasinghe <pradeep@climatesi.com>
2024-03-11 12:35:00 +0530

    banner

--- src/shared/service-proxies/service-proxies.ts
+++ src/shared/service-proxies/service-proxies.ts
@@ -20867,6 +20867,55 @@ export class InvestorToolControllerServiceProxy {
         return _observableOf(null as any);
     }
 
+    getDashboardAllDataGraph(): Observable<any> {
+        let url_ = this.baseUrl + "/investor-tool/dashboard-all-data-graph";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request("get", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processGetDashboardAllDataGraph(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processGetDashboardAllDataGraph(response_ as any);
+                } catch (e) {
+                    return _observableThrow(e) as any as Observable<any>;
+                }
+            } else
+                return _observableThrow(response_) as any as Observable<any>;
+        }));
+    }
+
+    protected processGetDashboardAllDataGraph(response: HttpResponseBase): Observable<any> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (response as any).error instanceof Blob ? (response as any).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+                result200 = resultData200 !== undefined ? resultData200 : <any>null;
+    
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf(null as any);
+    }
+
     getDashboardAllData(page: number, limit: number, filterText: string[]): Observable<any> {
         let url_ = this.baseUrl + "/investor-tool/dashboard-all-data?";
         if (page === undefined || page === null)
@@ -33347,15 +33396,6 @@ export interface IUpdateMethodologyAssessmentDto {
     [key: string]: any;
 }
 
-export enum DocumentsDocumentOwner {
-    Project = <any>"Project",
-    Country = <any>"Country",
-    CountryNC = <any>"CountryNC",
-    CountryBUR = <any>"CountryBUR",
-    CountryBTR = <any>"CountryBTR",
-    CountryNDC = <any>"CountryNDC",
-    CountryGHG = <any>"CountryGHG",
-}
 export class DataVerifierDto implements IDataVerifierDto {
     ids: number[];
     deadline: moment.Moment;
@@ -33434,7 +33474,15 @@ export interface IDataVerifierDto {
 
     [key: string]: any;
 }
-
+export enum DocumentsDocumentOwner {
+    Project = <any>"Project",
+    Country = <any>"Country",
+    CountryNC = <any>"CountryNC",
+    CountryBUR = <any>"CountryBUR",
+    CountryBTR = <any>"CountryBTR",
+    CountryNDC = <any>"CountryNDC",
+    CountryGHG = <any>"CountryGHG",
+}
 export class UpdateIndicatorDto implements IUpdateIndicatorDto {
     assessmentId: number;
     data: any[];
