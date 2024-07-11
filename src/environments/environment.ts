e953c4af1004b78d6b554c5a36cc0a0b38f13942
Pradeep_Ranasinghe <pradeep@climatesi.com>
2024-07-11 18:01:05 +0530

    email change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,10 @@
 
 export const environment = {
-  production: false,
-  baseUrlPMU: 'http://localhost:7080/pmu/login',
+  production: true,
   baseUrlAPI: 'https://tc-tool-mainservice-2sshj5de3a-ey.a.run.app',
   authBaseUrlAPI: 'https://tc-tool-authservice-2sshj5de3a-ey.a.run.app',
-  baseUrlAPIDocUploadAPI: 'http://localhost:7080/document/upload2',
-  baseUrlAuditlog: 'http://localhost:7000',
+  baseUrlAuditlog :  'https://tc-tool-auditservice-2sshj5de3a-ey.a.run.app',
+  baseUrlPMU: 'https://tc-tool-pmuweb-2sshj5de3a-ey.a.run.app',
   videoURL: 'https://sccv2.s3.ap-south-1.amazonaws.com/videos',
 
 };
